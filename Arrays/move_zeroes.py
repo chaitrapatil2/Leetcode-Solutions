b8a5ff33ef3b963f11023d579bdec7b3431d8d68
Chaitra Patil <your-chaitrap600@gmail.com>
2026-04-21 16:15:18 +0530

    Added Move Zeroes solution

---
+++ Arrays/move_zeroes.py
@@ -0,0 +1,14 @@
+
+def moveZeroes(nums):
+    insert_pos = 0
+
+    for num in nums:
+        if num != 0:
+            nums[insert_pos] = num
+            insert_pos += 1
+
+    while insert_pos < len(nums):
+        nums[insert_pos] = 0
+        insert_pos += 1
+
+    return nums
